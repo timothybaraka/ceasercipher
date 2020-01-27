f7131faac0a0502e71fe87f52b86d1afd77a51f9
timothybaraka <timothybaraka44@gmail.com>
2020-01-27 14:54:13 +0300

    Add a user input test

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,17 @@
+public class Cipher {
+    private String userInput;
+    private int userKey;
+
+    private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public Cipher(String userInput, int userKey){
+        this.userInput = userInput;
+        this.userKey = userKey;
+    }
+    public String getUserInput(){
+        return this.userInput;
+    }
+    public int getUserKey(){
+        return this.userKey;
+    }
+}
---
+++ src/test/java/CipherTest.java
@@ -0,0 +1,23 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CipherTest {
+
+    @Test
+    public void newCipher_instantiatesCorrectly_(){
+        Cipher testCipher = new Cipher("today", 4);
+        assertEquals(true , testCipher instanceof Cipher);
+    }
+
+    @Test
+    public void userInput_getsUserInput__String(){
+        Cipher testCipher= new Cipher("today", 4);
+        assertEquals("today" ,testCipher.getUserInput());
+    }
+
+    @Test
+    public void userKey_getsUserKey_Int(){
+        Cipher testCipher = new Cipher("today",4);
+        assertEquals(4,testCipher.getUserKey());
+    }
+}
